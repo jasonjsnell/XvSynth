548d1d333009cae99dedba16af14dee3939790c7
jasonjsnell <jasonjsnell@users.noreply.github.com>
2020-09-29 09:41:39 -0500

    Initial Commit

---
+++ XvSynth/XvSynth.h
@@ -0,0 +1,18 @@
+//
+//  XvSynth.h
+//  XvSynth
+//
+//  Created by Jason Snell on 9/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XvSynth.
+FOUNDATION_EXPORT double XvSynthVersionNumber;
+
+//! Project version string for XvSynth.
+FOUNDATION_EXPORT const unsigned char XvSynthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvSynth/PublicHeader.h>
+
+
