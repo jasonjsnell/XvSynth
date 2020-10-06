7e5302d6d73466b64ea05ff745925b04fe7143eb
jasonjsnell <jasonjsnell@users.noreply.github.com>
2020-10-06 11:46:32 -0500

    LFOs set up on main oscillator

---
+++ XvLFO.swift
@@ -0,0 +1,73 @@
+//
+//  XvLFO.swift
+//  XvSynth
+//
+//  Created by Jason Snell on 10/5/20.
+//
+
+import Foundation
+import XvUtils
+import XvDataMapping
+
+public class XvLFO {
+    
+    public var value:Double {
+        get { return wave.zeroBaseline(withPhaseShift: phaseShift) }
+    }
+    public var phaseShift:Double = 0.0
+    
+    
+    //MARK: Speed
+    fileprivate var _speed:Double = 1.0
+    
+    //set wave frequency with speed * multiplier
+    public var speed:Double {
+        get { return _speed }
+        set {
+            _speed = newValue
+            wave.frequency = _speed * _multiples[_multipier-1]
+        }
+    }
+    
+    //MARK: Multiplier
+    fileprivate let MULTIPLES_MIN:Int = 1
+    fileprivate let MULTIPLES_MAX:Int = 12
+    fileprivate let _multiples:[Double]
+    fileprivate var _multipier:Int = 1
+    fileprivate var _multiplierAttenuator:XvAttenuator
+    public var multiplier:Int {
+        get { return _multipier }
+        set {
+            _multipier = _multiplierAttenuator.attenuate(value: newValue)
+        }
+    }
+    
+    //MARK: Depth
+    //LFO depth is wave amplitude
+    public var depth:Double {
+        get {
+            return wave.amplitude
+        }
+        set {
+            wave.amplitude = newValue
+        }
+    }
+    
+    //an LFO requires a oscillator wave
+    public var wave:XvWave
+    
+    public init(wave:XvWave){
+        
+        self.wave = wave
+        
+        _multiplierAttenuator = XvAttenuator(min: Double(MULTIPLES_MIN), max: Double(MULTIPLES_MAX))
+        _multiples = [1] + Number.getGeometricSeries(exponent: 2.0, seriesLength: MULTIPLES_MAX)
+        
+        
+    }
+    
+    //pass to wave
+    public func refresh(){
+        wave.refresh()
+    }
+}
--- XvWaveSine.swift
+++ XvWaveSine.swift
@@ -8,108 +8,58 @@
 import Foundation
 import XvDataMapping
 
-public class XvWaveSine {
+//https://www.mathsisfun.com/algebra/amplitude-period-frequency-phase-shift.html
+
+/*
+ y = A sin(B(x + C)) + D
+ 
+ amplitude is A
+ period is 2π/B
+ phase shift is C (positive is to the left, negative to the right)
+ vertical shift is D
+ 
+ */
+
+public class XvWaveSine:XvWave {
     
     
     //MARK: - Accessors -
     
-    //standard -1.0 to 1.0
-    public var value:Double {
-        get { return _calculateSine() }
-    }
     
-    public func value(withPhaseShift:Double) -> Double {
-        return _calculateSine(withPhaseShift: withPhaseShift)
-    }
     
-    // instead of -1.0 to 1.0, it alters it 0.0-1.0
-    public var zeroBaseline:Double {
-        get { return (value + amplitude) / 2.0 }
-    }
     
-    //MARK: - Variables
-    //MARK: Time
-    //time interval needs to match the timeInterval of the Timer object that is firing / refreshing this object
-    //that way the seconds variable matches "real" time
-    fileprivate var _seconds:Double = 0.0
-    fileprivate var _timeInterval:Double = 0.1
+    
+    
     
     //MARK: Sine cycle
     //math constant
     fileprivate let SINE_CYCLE:Double = 2.0 * Double.pi
     
-    //MARK: Frequency
-    //if frequency is 1.0, then the sine wave will cycle one cycle per second
-    //faster frequencies require very fast timeIntervals / timer fires, otherwise it only catches the wave at 1.0 or 0.0 when it renders
-    //to get slow, LFO type sine waves, move the frequency down to 0.1 and lower
     
-    fileprivate var _frequency:Double // = 0.1
-    public var frequency:Double {
-        get { return _frequency }
-        set {
-            if (newValue > _timeInterval) {
-                print("XvSine: Error: If frequency is higher than the timeInterval, then the sine wave will lose resolution. For higher frequencies, create a faster Timer and timeInterval.")
-            }
-            _frequency = newValue
-        }
-    }
     
-    //MARK: Amplitude
-    //height of the wave
-    //default is 1.0, but can be any value
-    fileprivate var _amplitude:Double = 1.0
     
-    public var amplitude:Double {
-        get { return _amplitude }
-        set { _amplitude = newValue }
-    }
     
-    //MARK: Rounding
-    fileprivate var _roundTo:Double = 100000
+    
     
     //MARK: Init
     
-    public init(timeInterval:TimeInterval, amplitudeResistance:Double = 0.1, roundTo:Double = 10000){
+    public override init(timeInterval:TimeInterval, amplitudeResistance:Double = 0.1, roundTo:Double = 10000){
         
-        self._timeInterval = Double(timeInterval)
-        
-        //matching the frequency to the time interval will default the sine wave to go through one cycle per second
-        self._frequency = _timeInterval
-        
-        self._roundTo = roundTo
+        super.init(timeInterval: timeInterval, amplitudeResistance: amplitudeResistance, roundTo: roundTo)
         
     }
     
-    //MARK: - Refresh
-    //https://www.mathsisfun.com/algebra/amplitude-period-frequency-phase-shift.html
-    /*
-     y = A sin(B(x + C)) + D
-     
-     amplitude is A
-     period is 2π/B
-     phase shift is C (positive is to the left, negative to the right)
-     vertical shift is D
-     
-     */
-    public func refresh(){
-        
-        //update time interval
-        _seconds += _timeInterval
-        
-    }
+    
     
     //MARK: - calculations
     //current sine wave
-    fileprivate func _calculateSine() -> Double {
+    internal override func _generateWave() -> Double {
         
-        let sineWave:Double = _amplitude * sin(SINE_CYCLE * _frequency * _seconds)
-        
-        //round it
-        return Double(Int(sineWave * _roundTo)) / _roundTo
+        return _generateWave(withPhaseShift: 0)
     }
     
     //current wave with phase shift
-    fileprivate func _calculateSine(withPhaseShift:Double) -> Double {
+    internal override func _generateWave(withPhaseShift:Double) -> Double {
         
         let sineWave:Double = _amplitude * sin(SINE_CYCLE * _frequency * _seconds + withPhaseShift)
         
